df31e80a0ee37006bcc64ad71d175dc5216b3cb2
arsapoz <arsa@ultimatemovers.net>
2020-11-01 14:47:52 +0100

    Create autocomplete, adding and removing cards

---
+++ src/components/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <v-navigation-drawer v-model="drawer" app temporary>
+      <v-list dense> </v-list>
+    </v-navigation-drawer>
+    <v-app-bar app light>
+      <v-toolbar-title>Weather API</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+      <v-app-bar-nav-icon
+        class="hidden-md-and-up"
+        @click.stop="drawer = !drawer"
+      />
+    </v-app-bar>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Navbar",
+  data: () => ({
+    drawer: false
+  })
+});
+</script>
+<style>
+nav.v-navigation-drawer {
+  background: #0d4077 !important;
+}
+
+nav.v-navigation-drawer.v-navigation-drawer--fixed.v-navigation-drawer--is-mobile.v-navigation-drawer--open.v-navigation-drawer--temporary.theme--light {
+  background: #0d4077 !important;
+  color: white;
+}
+</style>
---
+++ src/components/Weather.vue
@@ -0,0 +1,172 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-toolbar dark color="teal">
+          <v-toolbar-title>City selection</v-toolbar-title>
+          <v-autocomplete
+            v-model="select"
+            :loading="loading"
+            :items="items"
+            item-text="name"
+            item-value="id"
+            :search-input.sync="search"
+            class="mx-4"
+            flat
+            hide-no-data
+            hide-details
+            label="What city are you from?"
+            solo-inverted
+          ></v-autocomplete>
+          <v-btn text @click="getWeatherByCity">
+            Add
+          </v-btn>
+        </v-toolbar>
+      </v-col>
+
+      <v-col cols="12" v-if="weatherCards.length">
+        <v-card
+          class="mx-auto mb-10"
+          max-width="400"
+          v-for="card in weatherCards"
+          :key="card.id"
+        >
+          <v-list-item two-line>
+            <v-list-item-content>
+              <v-list-item-title class="headline">
+                {{ card.name }}
+              </v-list-item-title>
+              <v-list-item-subtitle
+                >Mon, 12:30 PM, Mostly sunny</v-list-item-subtitle
+              >
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-card-text>
+            <v-row align="center">
+              <v-col class="display-3" cols="6">
+                23&deg;C
+              </v-col>
+              <v-col cols="6">
+                <v-img
+                  src="https://cdn.vuetifyjs.com/images/cards/sun.png"
+                  alt="Sunny image"
+                  width="92"
+                ></v-img>
+              </v-col>
+            </v-row>
+          </v-card-text>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-send</v-icon>
+            </v-list-item-icon>
+            <v-list-item-subtitle>23 km/h</v-list-item-subtitle>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-cloud-download</v-icon>
+            </v-list-item-icon>
+            <v-list-item-subtitle>48%</v-list-item-subtitle>
+          </v-list-item>
+
+          <!-- <v-slider
+              v-model="time"
+              :max="6"
+              :tick-labels="labels"
+              class="mx-4"
+              ticks
+          ></v-slider>
+
+          <v-list class="transparent">
+            <v-list-item
+                v-for="item in forecast"
+                :key="item.day"
+            >
+              <v-list-item-title>{{ item.day }}</v-list-item-title>
+
+              <v-list-item-icon>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-item-icon>
+
+              <v-list-item-subtitle class="text-right">
+                {{ item.temp }}
+              </v-list-item-subtitle>
+            </v-list-item>
+          </v-list>-->
+
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-btn text @click="removeCard(card.id)">
+              Remove card
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import axios from "axios";
+
+export default Vue.extend({
+  name: "Weather",
+  data: () => ({
+    weatherCards: [],
+    loading: false,
+    items: [],
+    search: null,
+    select: null,
+    states: [],
+    apiKey: "8041567580af3b158a42e66ef367b44a"
+  }),
+  watch: {
+    search(val) {
+      val && val !== this.select && this.querySelections(val);
+    }
+  },
+  mounted() {
+    this.getCities();
+  },
+  methods: {
+    removeCard(cardId) {
+      const i = this.weatherCards.map((card => card.id)).indexOf(cardId);
+      this.weatherCards.splice(i, 1);
+    },
+    getCities() {
+      axios.get("/current.city.list.min.json").then(resp => {
+        this.states = resp.data;
+      });
+    },
+    getWeatherByCity() {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?id=${this.select}&appid=${this.apiKey}`
+        )
+        .then(resp => {
+          this.weatherCards.push(resp.data);
+        })
+        .finally(() => {
+          this.search = null;
+          this.select = null;
+        });
+    },
+    querySelections(value) {
+      this.loading = true;
+      // Simulated ajax query
+      this.items = this.states.filter(state => {
+        return (
+          (state.name || "")
+            .toLowerCase()
+            .indexOf((value || "").toLowerCase()) > -1
+        );
+      });
+      this.loading = false;
+    }
+  }
+});
+</script>
